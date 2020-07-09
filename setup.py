b53025d594fdf5ba4d478a0b7bc0676d4ebf0138
xing1999 <xing@cinnamon.is>
2020-07-09 10:21:54 +0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+import platform
+
+from setuptools import find_packages, setup, find_namespace_packages
+
+
+def torch_urls(version):
+    platform_system = platform.system()
+    if platform_system == "Windows":
+        return f"torch@https://download.pytorch.org/whl/cu90/torch-{version}-cp36-cp36m-win_amd64.whl#"
+    return f"torch=={version}"
+
+
+setup(
+    name="lib-poolnet",
+    version="v0.2.2",
+    description="Ready-to-use Saliency object detection under one common API",
+    author="Cinnamon AI Lab",
+    url="https://github.com/xing1999/PoolNet",
+    packages=find_namespace_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    python_requires=">=3.6",
+    install_requires=[torch_urls("1.4"), "numpy"],
+    extras_require={
+        "test": ["pytest"],
+        "dev": ["black"],
+    },
+)
