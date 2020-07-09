bfd3cd23313e786b0ee9b632a11204fa821b407b
xing1999 <xing@cinnamon.is>
2020-07-09 19:05:00 +0700

    add unit test

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,3 +6,11 @@ def test_encoder():
     encoder = Encoder()
     x = torch.rand(size=(1, 3, 224, 224))
     encoder(x)
+
+def test_model():
+    from pool_net import VggPoolNet
+    model = VggPoolNet()
+    x = torch.rand(size=(1, 3, 224, 224))
+    model(x)
+    
+
