c52d8fd4fc8844fe9898a2b04efd8d47e1db6fb0
xing1999 <xing@cinnamon.is>
2020-07-21 15:57:21 +0700

    add augmentation

---
+++ pool_net/datasets/transforms.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageOps
+import random
+
+class HorizontalFlipSegmentation():
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img, mask):
+        c_p = random.random()
+
+        if c_p < p:
+            return img, mask
+
+        img = ImageOps.mirror(img)
+        mask = ImageOps.mirror(mask)
+
+        return img, mask
+
--- pool_net/main.py
+++ pool_net/main.py
@@ -21,7 +21,6 @@ from torchvision import transforms
 def get_train_transform():
     return transforms.Compose(
         [
-            transforms.RandomHorizontalFlip(0.5),
             transforms.ToTensor(),
             transforms.Normalize(
                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
